bb1fbce4cda1ec5362dbeb03ba47d25d0aeb2064
MilaTrainer <lyudmilaalekhinakz@gmail.com>
2023-05-03 19:12:07 -0700

    3js

---
+++ homework_3/tasck.3.5/scripts.js
@@ -0,0 +1,22 @@
+'use strict'
+/*
+Задание 5
+
+Дан массив объектов, например:
+
+
+
+let questions = [{
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0
+}, {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0
+}];
+
+Добавить в каждый объект дополнительное поле usersAnswer со значением null.
+
+Решение должно работать для массива любой длины.
+*/

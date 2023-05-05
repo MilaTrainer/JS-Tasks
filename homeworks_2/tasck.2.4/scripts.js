8b98c3f2343378f573a83c7bddf4da66a79dfac5
MilaTrainer <lyudmilaalekhinakz@gmail.com>
2023-05-04 18:28:26 -0700

    2js

---
+++ homeworks_2/tasck.2.4/scripts.js
@@ -0,0 +1,28 @@
+'use strict'
+/*
+Задание 4
+Вывести в консоль квадраты чисел 
+от 0 до 100 (не включая 100) 
+- т.е. вывести числа 0, 1, 4, 9, 16...
+
+* Задавать количество чисел, которые нужно вывести, 
+при помощи функции prompt.
+*/
+
+
+
+const num = prompt("How many numbers' squares you want to calculate 0 - 10?");
+for (let i = 0; i < 100; i++) {
+    let result = i ** 2;
+    if (i < num && result) {
+        console.log(result);
+    }
+}
+
+
+
+
+// i = Number(i);
+
+
+//
---
+++ homeworks_2/tasck2.1/scripts.js
@@ -0,0 +1,16 @@
+'use strict';
+//Перепишите код с использованием тернарного оператора
+
+
+/*let country = 'Sweden';
+ let access;
+ if (country == 'Sweden') {
+     access = true;
+ } else {
+     access = false;
+ }
+console.log(access);*/
+
+let country = 'Sweden';
+let access = 'Sweden' ? true : false;
+console.log(access);
